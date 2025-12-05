e103eb9c58524dbb0247e3201a10310fb1eb0781
minhthinh27 <minhthinh.phng@gmail.com>
2025-12-05 10:03:42 +0700

    update config

--- config/bootstrap.go
+++ config/bootstrap.go
@@ -1,12 +1,12 @@
 package gdconfig
 
 type IBaseConfig interface {
-	GetEnv() *Env
-	GetServer() *Server
-	GetDatabase() *Database
-	GetCache() *Cache
-	GetTime() *Time
-	GetQueue() *Queue
+	GetEnv() Env
+	GetServer() Server
+	GetDatabase() Database
+	GetCache() Cache
+	GetTime() Time
+	GetQueue() Queue
 }
 
 type BaseConfig struct {
@@ -18,7 +18,7 @@ type BaseConfig struct {
 	queue    Queue
 }
 
-func InitBase() IBaseConfig {
+func Init() IBaseConfig {
 	return &BaseConfig{}
 }
 
@@ -70,26 +70,26 @@ type Queue struct {
 	GroupId string
 }
 
-func (c *BaseConfig) GetEnv() *Env {
-	return &c.env
+func (c *BaseConfig) GetEnv() Env {
+	return c.env
 }
 
-func (c *BaseConfig) GetServer() *Server {
-	return &c.server
+func (c *BaseConfig) GetServer() Server {
+	return c.server
 }
 
-func (c *BaseConfig) GetDatabase() *Database {
-	return &c.database
+func (c *BaseConfig) GetDatabase() Database {
+	return c.database
 }
 
-func (c *BaseConfig) GetCache() *Cache {
-	return &c.cache
+func (c *BaseConfig) GetCache() Cache {
+	return c.cache
 }
 
-func (c *BaseConfig) GetTime() *Time {
-	return &c.timer
+func (c *BaseConfig) GetTime() Time {
+	return c.timer
 }
 
-func (c *BaseConfig) GetQueue() *Queue {
-	return &c.queue
+func (c *BaseConfig) GetQueue() Queue {
+	return c.queue
 }
